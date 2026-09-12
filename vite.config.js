8be4071ee0c98cb3218f113bbf97b18308a55236
Br174 <brunoverlezza@gmail.com>
2026-09-12 23:28:52 +0200

    feat: activate v0.9 hierarchy parser

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: './lib/documentParserV08.js',
+        replacement: fileURLToPath(new URL('./src/lib/documentParserV09.js', import.meta.url)),
+      },
+    ],
+  },
+});
