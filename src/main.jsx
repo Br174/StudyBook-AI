8cd14e17836516d73ef0171268058ded4a17c66f
Br174 <brunoverlezza@gmail.com>
2026-09-12 22:55:37 +0200

    feat: enable StudyBook AI v0.7 library workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import AppV07 from './AppV07.jsx';
 import './styles.css';
+import './library.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppV07 />
   </React.StrictMode>,
 );
