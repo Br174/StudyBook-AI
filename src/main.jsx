d8572f5daad0772d5e666e8487a2abfbd6adc0ba
Br174 <brunoverlezza@gmail.com>
2026-09-13 00:18:04 +0200

    feat: activate StudyBook AI v0.11

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppV08 from './AppV08.jsx';
+import AppV11 from './AppV11.jsx';
 import './styles.css';
 import './library.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppV08 />
+    <AppV11 />
   </React.StrictMode>,
 );
